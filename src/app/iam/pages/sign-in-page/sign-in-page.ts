b40478de6750081a36bb3cbb78e72144831837b5
Valentino Cervantes Erequita <104030868+KiwiAmenazante@users.noreply.github.com>
2025-10-04 01:02:22 -0500

    feat: enhance sign-in page with background image and integrate SignInFormComponent

--- src/app/iam/pages/sign-in-page/sign-in-page.ts
+++ src/app/iam/pages/sign-in-page/sign-in-page.ts
@@ -1,29 +1,17 @@
 import {Component, OnInit} from '@angular/core';
-import {IamService} from '../../services/iam.service';
+import {SignInFormComponent} from '../../components/sign-in-form-component/sign-in-form-component';
 
 @Component({
   selector: 'app-sign-in-page',
-  imports: [],
+  imports: [
+    SignInFormComponent
+  ],
+  standalone: true,
   templateUrl: './sign-in-page.html',
   styleUrl: './sign-in-page.css'
 })
-export class SignInPage implements OnInit {
+export class SignInPage {
 
-  constructor(
-    private iamService: IamService
-  ) {
-  }
-
-   ngOnInit(): void {
-    this.iamService.SignIn({username: 'Dedalus', password: 'Admin123admin'}).subscribe({
-     next: (data) => {
-       console.log(data);
-       },
-       error: (error) => {
-         console.error('There was an error!', error);
-       }
-     });
-  }
 
 
 
