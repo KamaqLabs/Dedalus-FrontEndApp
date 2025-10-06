e8fe8791d7b6ff55aae96ab0760e979ff3e9b5c6
Valentino Cervantes Erequita <104030868+KiwiAmenazante@users.noreply.github.com>
2025-10-05 22:30:49 -0500

    feat: add DashboardPage component with HTML, CSS, and basic session storage logic

---
+++ src/app/dashboard/pages/dashboard-page/dashboard-page.ts
@@ -0,0 +1,24 @@
+import {Component, OnInit} from '@angular/core';
+import {SessionStorageService} from '../../../public/services/session-storage.service';
+
+@Component({
+  selector: 'app-dashboard-page',
+  imports: [],
+  templateUrl: './dashboard-page.html',
+  styleUrl: './dashboard-page.css'
+})
+export class DashboardPage implements OnInit {
+
+
+    constructor(
+      private sessionService: SessionStorageService,
+      private administratorProfileService: SessionStorageService,
+    ) {}
+
+    ngOnInit(): void {
+
+      console.log(this.sessionService.get("profile"));
+
+    }
+
+}
