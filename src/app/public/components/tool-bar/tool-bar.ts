9247895e7abd0364ffd2428894f18719e3451be2
Valentino Cervantes Erequita <104030868+KiwiAmenazante@users.noreply.github.com>
2025-10-06 22:21:23 -0500

    feat: Refactor toolbar navigation and add logout functionality
    Co-Authored-By: Eduardo Sihuar Ccotarma Ttito <116849769+Anx0123@users.noreply.github.com>
    Co-Authored-By: Gabriel Anthony Braithuaite Toledo <134438310+Gaboo04@users.noreply.github.com>
    Co-Authored-By: MiguelCarpioC <189032266+MiguelCarpioC@users.noreply.github.com>
    Co-Authored-By: Mathifaa519 <206495024+Mathifaa519@users.noreply.github.com>

--- src/app/public/components/tool-bar/tool-bar.ts
+++ src/app/public/components/tool-bar/tool-bar.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import {SessionStorageService} from '../../services/session-storage.service';
+import {IamService} from '../../../iam/services/iam.service';
+import {Administrator} from '../../../profiles/models/administrator.entity';
+import {SnackbarErrorService} from '../../services/snackbar-error.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-tool-bar',
@@ -8,9 +12,37 @@ import {SessionStorageService} from '../../services/session-storage.service';
   styleUrl: './tool-bar.css'
 })
 export class ToolBar {
-  constructor(private sessionStorageService: SessionStorageService) {}
+  constructor(
+    private sessionStorageService: SessionStorageService,
+    private iamService: IamService,
+    private snackbarError: SnackbarErrorService,
+    private router: Router
+    ) {}
 
   isAuthenticated(): boolean {
-    return this.sessionStorageService.get<boolean>('isAuthenticated') === true;
+    return this.sessionStorageService.authenticated();
+  }
+
+  logout(): void {
+
+    const profile: Administrator | null = this.sessionStorageService.getProfile();
+
+    if(!profile)
+    {
+      this.snackbarError.show("usuario invalido", 4000);
+      return
+    }
+    this.iamService.LogOut(profile.accountId).subscribe({
+      next: () => {
+        this.router.navigate(['/']);
+        this.sessionStorageService.clear();
+      },
+      error: (err) => {
+        console.log(err);
+        this.snackbarError.show(err, 4000);
+      }
+    });
+
+
   }
 }
