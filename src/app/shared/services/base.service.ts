2158495cec486386acb1c7b3e8587f987e827c8f
Valentino Cervantes Erequita <104030868+KiwiAmenazante@users.noreply.github.com>
2025-11-23 17:12:24 -0500

    feat: update file replacements in angular.json for production environment

--- src/app/shared/services/base.service.ts
+++ src/app/shared/services/base.service.ts
@@ -1,7 +1,7 @@
 import {HttpClient, HttpErrorResponse, HttpHeaders} from "@angular/common/http";
 import {inject} from "@angular/core";
 import {catchError, Observable, retry, throwError} from "rxjs";
-import {environment} from "../../../environments/environment.development";
+import {environment} from "../../../environments/environment";
 
 export class BaseService <T> {
 
