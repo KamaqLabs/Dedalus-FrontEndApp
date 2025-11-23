fe154919293aa775bd15baf8441043bc9cf5534e
Valentino Cervantes Erequita <104030868+KiwiAmenazante@users.noreply.github.com>
2025-11-23 16:37:34 -0500

    feat: update serverBasePath for production environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  serverBasePath:'http://localhost:3000/api/v1/',
+  serverBasePath:'https://sogw0gwg8w0w8ok8gkgwsso0.4.201.187.236.sslip.io/api/v1/',
   edgeServerBasePath:'http://localhost:5000/api/v1/',
 };
